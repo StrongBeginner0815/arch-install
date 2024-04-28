71db5773d12f67a38fbb2e53e9cc9b7bcc0a25d3
StrongBeginner0815 <143025318+StrongBeginner0815@users.noreply.github.com>
2024-04-28 14:02:43 +0200

    Update tmp.sh

--- tmp.sh
+++ tmp.sh
@@ -1,5 +1,5 @@
 echo "chroot (change root) in die Mount-Partition zum Anpassen fehlender Konfigurationen: ###############"
-arch-chroot /mnt ./install-step-3.sh
+arch-chroot /mnt ls #./install-step-3.sh
 
 echo "Aus der chroot-Umgebung austreten & gemountete Partitionen unmounten... Warte auf ein ENTER..."
 read user_input
