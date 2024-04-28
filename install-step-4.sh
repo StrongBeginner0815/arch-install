26c7e672d9210fe7a22e1fd46d885d6924fe5511
StrongBeginner0815 <143025318+StrongBeginner0815@users.noreply.github.com>
2024-04-28 14:31:26 +0200

    Create install-step-4.sh

---
+++ install-step-4.sh
@@ -0,0 +1,8 @@
+#! /usr/bin/bash
+
+# 4. Teil des Installations-Scriptes
+# auszuführen auf dem laufenden System
+
+
+echo "Schritt 4.1 - benötigte Pakete installieren: ##############################################################"
+pacman -S xfce4 xfce4-goodies --noconfirm
