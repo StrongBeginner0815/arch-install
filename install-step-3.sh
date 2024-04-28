a2a3c62b717ba9e407d019cc3db2cf5278f84bbf
StrongBeginner0815 <143025318+StrongBeginner0815@users.noreply.github.com>
2024-04-28 17:22:20 +0200

    Update install-step-3.sh

--- install-step-3.sh
+++ install-step-3.sh
@@ -3,12 +3,12 @@
 # 3. Teil des Installations-Scriptes
 # siehe Anleitung: https://wiki.archlinux.de/title/Anleitung_f%C3%BCr_Einsteiger
 
-echo "Dieses Script liegt im chroot und konfiguriert das System von innen heraus."
-echo "Warte auf ENTER"
-read waiting
+echo "Dieses Script wird per chroot auf einem System-Sklett aufgerufen und konfiguriert das System von innen heraus. Falls dem nicht so ist: Strg+C !"
+echo "Bitte gewünschten Hostnamen eingeben!"
+read hostname
 
 echo "Hostname setzen..."
-echo HOSTNAME > /etc/hostname
+echo "$hostname" > /etc/hostname
 
 echo "Sprache einstellen..."
 echo LANG=de_DE.UTF-8 > /etc/locale.conf
@@ -24,11 +24,11 @@ echo FONT=lat9w-16 >> /etc/vconsole.conf
 echo "Zeitzone festlegen (symbolischer Link)..."
 ln -sf /usr/share/zoneinfo/Europe/Berlin /etc/localtime
 
-echo "localhosts einrichten ÜBERSPRINGEN"
-# echo -e "#<ip-address>	<hostname.domain.org>	<hostname>\n127.0.0.1	localhost.localdomain	localhost\n::1		localhost.localdomain	localhost" > /etc/hosts
+echo "localhosts einrichten..."
+echo -e "#<ip-address>	<hostname.domain.org>	<hostname>\n127.0.0.1	localhost.localdomain	localhost\n::1		localhost.localdomain	localhost" > /etc/hosts
 
-echo "Initramfs erneut erzeugen, z.B. falls LVM eingerichtet wird, wie ist das bei Microcode?, /etc/kminitcpio.conf prüfen! ÜBERSPRINGEN"
-# mkinitcpio -p linux
+echo "Initramfs erneut erzeugen, z.B. falls LVM eingerichtet wird, wie ist das bei Microcode?, /etc/kminitcpio.conf prüfen!"
+mkinitcpio -p linux
 
 echo "root-Password setzen:"
 echo "Bitte gewünschtes Passwort eingeben"
@@ -36,13 +36,10 @@ read passwd_choice
 echo $passwd_choice | passwd root -s
 
 echo "GRUB installieren (es gibt Alternativen!); os-prober sorgt dafür, dass andere BS automatisch eingerichtet werden..."
-pacman -S grub efibootmgr os-prober --noconfirm
+pacman -S os-prober grub efibootmgr --noconfirm
 
 echo "UEFI-Einträge einrichten (für BIOS siehe https://wiki.archlinux.de/title/Grub#Installation)..."
 echo $(grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id="ARSCH - Mein Gans persönlicher GRUB-Bootloader")
 echo $(grub-mkconfig -o /boot/grub/grub.cfg)
-
-echo "Script inside chroot is ready. Drücke Enter!"
-read wait_for_enter
 
 exit 0
