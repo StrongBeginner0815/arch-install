211b429bfe8660698e9441309fab57dbc52e1fc2
StrongBeginner0815 <143025318+StrongBeginner0815@users.noreply.github.com>
2024-04-28 13:59:34 +0200

    Create tmp.sh

---
+++ tmp.sh
@@ -0,0 +1,13 @@
+echo "chroot (change root) in die Mount-Partition zum Anpassen fehlender Konfigurationen: ###############"
+arch-chroot /mnt ./install-step-3.sh
+
+echo "Aus der chroot-Umgebung austreten & gemountete Partitionen unmounten... Warte auf ein ENTER..."
+read user_input
+
+exit
+umount /mnt/boot
+umount /mnt
+
+echo "FERTIG! Das System sollte jetzt mit dem Befehl   reboot   neugestartet werden."
+
+exit 0
