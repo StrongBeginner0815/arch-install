adfeeb53a3450b64c7f6aefd7e8d1a15f6a69753
StrongBeginner0815 <143025318+StrongBeginner0815@users.noreply.github.com>
2024-04-29 21:43:49 +0200

    Update install-step-3.sh

--- install-step-3.sh
+++ install-step-3.sh
@@ -5,6 +5,10 @@
 
 echo "Dieses Script wird per chroot auf einem System-Sklett aufgerufen und konfiguriert das System von innen heraus. Falls dem nicht so ist: Strg+C !"
 
+
+echo "Sind wir wirklich im chroot? Folgendes ist die Antwort des ls-Befehls (-> welche sh-Dateien sind vorhanden?):"
+ls
+
 echo "Hostname setzen..."
 echo "Bitte gewünschten Hostnamen eingeben!"
 read hostname
@@ -24,27 +28,32 @@ echo FONT=lat9w-16 >> /etc/vconsole.conf
 echo "Zeitzone festlegen (symbolischer Link)..."
 ln -sf /usr/share/zoneinfo/Europe/Berlin /etc/localtime
 
-echo "localhosts einrichten..."
+echo "(local)hosts einrichten..."
 echo -e "#<ip-address>	<hostname.domain.org>	<hostname>\n127.0.0.1	localhost.localdomain	localhost\n::1		localhost.localdomain	localhost" > /etc/hosts
 
-echo "Initramfs erneut erzeugen, z.B. falls LVM eingerichtet wird, wie ist das bei Microcode?, /etc/kminitcpio.conf prüfen!"
-mkinitcpio -p linux
 
 echo "root-Password setzen:"
 echo "Bitte gewünschtes Passwort eingeben"
 read passwd_choice
 echo $passwd_choice | passwd root -s
 
-echo "GRUB & weitere Pakete (DM, DE, GPU) installieren (es gibt Alternativen!); os-prober sorgt dafür, dass andere BS automatisch eingerichtet werden..."
+echo "GRUB + DisplayManager + DesktopEnvironment + GPUbase_driver + Tools installieren (es gibt Alternativen!); os-prober sorgt dafür, dass andere BS automatisch eingerichtet werden..."
 # Mithilfe folgenden Befehls schonmal weitere wichtige Pakete installieren:
 # Microcode statt "intel-ucode" bei AMD: "amd-ucode"
 # Für WLAN-Verbindung zusätzlich "iwd"
-pacman -S os-prober grub efibootmgr posix intel-ucode networkmanager xfce4 xfce4-goodies xorg-xdm xorg-server xorg-xinit cups cups-pdf xf86-video-vesa --noconfirm
+pacman -S os-prober grub efibootmgr posix intel-ucode networkmanager xfce4 xfce4-goodies xorg-xdm xorg-server cups cups-pdf xf86-video-vesa --noconfirm
 #-----------------------------------------------------------------------------------> Fehler: /proc muss gemounted sein!
+
+echo "Sind wir wirklich im chroot? Folgendes ist die Antwort des ls-Befehls (-> welche sh-Dateien sind vorhanden?):"
+ls
 
 echo "UEFI-Einträge einrichten (für BIOS siehe https://wiki.archlinux.de/title/Grub#Installation)..."
 echo $(grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id="ARSCH - Mein Gans persönlicher GRUB-Bootloader")
 echo $(grub-mkconfig -o /boot/grub/grub.cfg)
+
+
+echo "Initramfs erneut erzeugen, z.B. falls LVM eingerichtet wird, wie ist das bei Microcode?, /etc/kminitcpio.conf prüfen!"
+mkinitcpio -p linux
 
 echo "NetworkManager aktivieren"
 systemctl enable NetworkManager.service
